9a2b41a89eeff8f4fe9fcdf23ee926f60daa4187
Arnaud Le Blanc <arnaud.lb@gmail.com>
2010-08-15 19:21:07 +0000

    * allow subscript expressions in tags
    git-svn-id: https://zwig.googlecode.com/svn/trunk@8 0f079118-c391-7bd7-68ef-ca332430dac1

--- lib/Zwig/TokenParser/ViewHelper.php
+++ lib/Zwig/TokenParser/ViewHelper.php
@@ -3,7 +3,7 @@
 class Zwig_TokenParser_ViewHelper extends Twig_TokenParser
 {
     /*
-     * Parses {% someZendViewHelper (arg1, arg2, ...) %}
+     * Parses {% someZendViewHelper (arg1, arg2, ...) %} with optional subscript expression
      */
     public function parse(Twig_Token $token)
     {
@@ -11,10 +11,12 @@ class Zwig_TokenParser_ViewHelper extends Twig_TokenParser
 
         $args = $this->parser->getExpressionParser()->parseArguments();
 
+        $node = new Zwig_Node_Expression_ViewHelper($helper, $args, $token->getLine(), $this->getTag());
+        $node = $this->parser->getExpressionParser()->parseSubscriptExpression($node);
+
         $this->parser->getStream()->expect(Twig_Token::BLOCK_END_TYPE);
 
-        $expr = new Zwig_Node_Expression_ViewHelper($helper, $args, $token->getLine(), $this->getTag());
-        return new Zwig_Node_Stmt($expr, array(), $token->getLine(), $this->getTag());
+        return new Zwig_Node_Stmt($node, array(), $token->getLine(), $this->getTag());
     }
 
     public function getTag() 
