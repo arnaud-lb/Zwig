e2cebab399dc836ab3c01fa971799f71f6ecc0f4
Arnaud Le Blanc <arnaud.lb@gmail.com>
2010-12-05 11:24:52 +0000

    lazy-load parser
    git-svn-id: https://zwig.googlecode.com/svn/trunk@26 0f079118-c391-7bd7-68ef-ca332430dac1

--- lib/Zwig/Environment.php
+++ lib/Zwig/Environment.php
@@ -6,12 +6,18 @@ class Zwig_Environment extends Twig_Environment
 
     public function __construct(Zwig_View $view, Twig_LoaderInterface $loader = null, $options = array(), Twig_LexerInterface $lexer = null, Twig_ParserInterface $parser = null, Twig_CompilerInterface $compiler = null) {
 
-        $parser = $parser !== null ? $parser : new Zwig_Parser($this);
-
         parent::__construct($loader, $options, $lexer, $parser, $compiler);
 
         $this->addExtension(new Zwig_Extension);
         $this->view = $view;
+    }
+
+    public function getParser()
+    {
+        if (null === $this->parser) {
+            $this->parser = new Zwig_Parser($this);
+        }
+        return $this->parser;
     }
 
     public function loadTemplate($template)
