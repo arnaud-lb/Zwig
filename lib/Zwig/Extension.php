6d873bea85557b2c6ef0142947960288987aa389
Arnaud Le Blanc <arnaud.lb@gmail.com>
2010-12-30 18:04:38 +0100

    cleanup

--- lib/Zwig/Extension.php
+++ lib/Zwig/Extension.php
@@ -24,15 +24,6 @@ class Zwig_Extension extends Twig_Extension
         return array($broker);
     }
 
-    public function getFilters()
-    {
-        $filters = array(
-            'js' => new Twig_Filter_Function('json_encode', array('is_safe' => array('js'))),
-            'string' => new Zwig_Filter_Cast('string'),
-        );
-        return $filters;
-    }
-
     public function getName()
     {
         return 'Zwig';
